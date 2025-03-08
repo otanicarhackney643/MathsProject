dc31989e29e62575d351000cd02996bf26c791aa
otanicarhackney643 <otanicarhackney643@gmail.com>
2025-03-08 20:49:34 +0000

    Add 3.php

---
+++ src/3.php
@@ -0,0 +1,11 @@
+<?php
+function factorial($n) {
+    if ($n == 0 || $n == 1) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+echo factorial(5); // Output: 120
+?>
