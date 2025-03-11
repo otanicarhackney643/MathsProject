6c60c1538fbe5c4350cf04974dbd993fb2417887
otanicarhackney643 <otanicarhackney643@gmail.com>
2025-03-11 15:47:08 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,11 @@
+<?php
+// Get two numbers from the user
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+
+// Calculate the sum of the two numbers
+$sum = $num1 + $num2;
+
+// Display the result
+echo "The sum is: $sum";
+?>
