b6d9174849adda86335421e383f5bfe54f8b02e2
otanicarhackney643 <otanicarhackney643@gmail.com>
2025-03-17 23:21:52 +0000

    Add 12.php

---
+++ src/12.php
@@ -0,0 +1,10 @@
+ <?php
+     $num = rand(1, 10);
+     echo "The number is ".$num."</br>";
+     
+     if($num%2==0){
+         echo "Even Number";
+     }else{
+         echo "Odd Number";
+     }
+ ?>
