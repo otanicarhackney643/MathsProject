0844baa7ee652d663050b17cc63de4787b27e012
otanicarhackney643 <otanicarhackney643@gmail.com>
2025-05-23 06:34:34 +0000

    Add 39.php

---
+++ src/39.php
@@ -0,0 +1,12 @@
+<?php
+    $data = [
+        "name" => "John",
+        "age" => 25,
+        "city" => "New York"
+    ];
+
+    foreach ($data as $key => $value) {
+        echo "$key is set to: $value\n";
+    }
+
+?>
